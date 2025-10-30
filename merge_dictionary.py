b3697af403987881f3db70307ce51b87d7959a5b
Subhajit Mondal <75659257+MSubhajitIND@users.noreply.github.com>
2025-10-30 15:16:33 +0530

    Merge Dictionary

---
+++ merge_dictionary.py
@@ -0,0 +1,8 @@
+#Write a python script to merge two python dictionary
+
+dict1 = {"a": 1, "b": 2}
+dict2 = {"c": 3, "d": 4}
+
+merged_dict = dict1.copy() 
+merged_dict.update(dict2)
+print("Merged Dictionary:", merged_dict)
